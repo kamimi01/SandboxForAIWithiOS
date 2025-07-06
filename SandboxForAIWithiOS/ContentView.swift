6ed21b3d503b68509e0518df5ece41a41339d1a1
kamimi01 <awakaru0122@gmail.com>
2025-07-06 15:32:46 +0200

    Initial Commit

---
+++ SandboxForAIWithiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SandboxForAIWithiOS
+//
+//  Created by mika_admin on 2025-07-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SandboxForAIWithiOS/SandboxForAIWithiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxForAIWithiOSApp.swift
+//  SandboxForAIWithiOS
+//
+//  Created by mika_admin on 2025-07-06.
+//
+
+import SwiftUI
+
+@main
+struct SandboxForAIWithiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
